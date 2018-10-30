cd2f6bd0023af906ff5004d451a448f71d11daf7
Carlos <43774751+CFigueirasV@users.noreply.github.com>
2018-10-30 09:56:18 +0100

    perfe

--- Boletin5/src/boletin5/Boletin5.java
+++ Boletin5/src/boletin5/Boletin5.java
@@ -10,43 +10,43 @@ import java.util.Scanner;
 public class Boletin5 {
 public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        Conta contaOrigen = new Conta("Roberto Merino", "1", 1050);
-        Conta contaDestino = new Conta();
-        contaDestino.setNome("Ricardo Méndez");
-        contaDestino.setNumConta("2");
-        contaDestino.setSaldo(2600);
-        contaOrigen.visualizar();
-        contaDestino.visualizar();
-        contaOrigen.ingreso(2000);
-        contaDestino.reintegro(600);
-        contaOrigen.visualizar();
-        contaDestino.visualizar();
+        Cuenta cuentaOrigen = new Cuenta("Mikel Merino", "1", 1050);
+        Cuenta cuentaDestino = new Cuenta();
+        cuentaDestino.setNome("Kepa Arrizabalaga");
+        cuentaDestino.setNumConta("2");
+        cuentaDestino.setSaldo(2600);
+        cuentaOrigen.visualizar();
+        cuentaDestino.visualizar();
+        cuentaOrigen.ingreso(2000);
+        cuentaDestino.reintegro(600);
+        cuentaOrigen.visualizar();
+        cuentaDestino.visualizar();
 
-        System.out.println("\nIndique la cantidad a transferir: ");
+        System.out.println("\nIndica la cantidad a transferir: ");
         double cant = ler.nextDouble();
         while (cant <= 0) {
             System.out.println("ERROR DE TRANSFERENCIA");
-            System.out.println("\nIndique la cantidad a transferir: ");
+            System.out.println("\nIndica la cantidad a transferir: ");
             cant = ler.nextDouble();
         }
 
-        if (contaDestino.transferencia(contaOrigen, cant) == true) {
+        if (cuentaDestino.transferencia(cuentaOrigen, cant) == true) {
             System.out.println("\nTransferencia realizada");
         } else {
             System.out.println("ERROR DE TRANSFERENCIA");
         }
-        contaOrigen.visualizar();
-        contaDestino.visualizar();
+        cuentaOrigen.visualizar();
+        cuentaDestino.visualizar();
 
         System.out.println("\nIndique la cantidad a transferir: ");
         cant = ler.nextDouble();
-        if (contaDestino.transferencia(contaOrigen, cant) == true) {
+        if (cuentaDestino.transferencia(cuentaOrigen, cant) == true) {
             System.out.println("\nTransferencia realizada");
         } else {
             System.out.println("ERROR DE TRANSFERENCIA");
         }
-        contaOrigen.visualizar();
-        contaDestino.visualizar();
+        cuentaOrigen.visualizar();
+        cuentaDestino.visualizar();
 
     }
 }
